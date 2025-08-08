51b9bf730e73d3d4fdce8ae4f2218772893695c5
Svetlana Kapelko <56638107+Svkapelko@users.noreply.github.com>
2025-08-08 13:15:42 +0300

    Update products.js
    Add conslole.log

--- products.js
+++ products.js
@@ -10,7 +10,7 @@ function showProducts() {
 <p><a href="/add.html?product=${item}">Добавить отзыв</a></p>
 <p><a href="/reviews.html?product=${item}">Посмотреть отзывы</a></p>
 		`;
-
+console.log('Добавляем инфо для курса по git')
 	});
 };
 
